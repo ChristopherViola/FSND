9036676597b4d46ab8bb864f638068bf9c778262
ChristopherViola <christopher.viola.93@gmail.com>
2018-02-12 14:43:20 +0100

    Update send_sms.py

--- Lesson 7 - Send sms/send_sms.py
+++ Lesson 7 - Send sms/send_sms.py
@@ -1,8 +1,8 @@
 from twilio.rest import Client
 
 # Find these values at https://twilio.com/user/account
-account_sid = "ACacbecc3511b92bb37f085a3a4e7fe0a3"
-auth_token = "9695de289b3e1b0751b76ee023d3df1a"
+account_sid = "ACacbecc3511b92bb37f0***"
+auth_token = "9695de289b3e1b0751***"
 
 client = Client(account_sid, auth_token)
 
