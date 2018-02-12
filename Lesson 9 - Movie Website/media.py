209f5940320846a00f4e9ebf8c9b239bee1c336c
ChristopherViola <christopher.viola.93@gmail.com>
2018-02-12 19:15:05 +0100

    update

--- Lesson 9 - Movie Website/media.py
+++ Lesson 9 - Movie Website/media.py
@@ -2,5 +2,5 @@ class Movie():
     def __init__(self, movie_title, movie_storyline, poster_image, trailer_youtube):
         self.title = movie_title
         self.story_line = movie_storyline
-        self.poster_url = poster_image
-        self.trailer_url = trailer_youtube
+        self.poster_image_url = poster_image
+        self.trailer_youtube_url = trailer_youtube
